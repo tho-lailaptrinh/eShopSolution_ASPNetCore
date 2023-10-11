aee119c1e9849a6cebbeecdc1b6168101ed22548
thọ thọ <vuvantho190604@gmail.com>
2023-10-11 20:38:12 +0700

    Create login jwt

---
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.ViewModels.System;
+using eShopSolution.ViewModels.System.Logins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public interface IUserService
+    {
+        // Tạo ra 1 Task<> chả về có đăng nhập đc hay không
+        Task<string> Authencate(LoginRequest request);
+        Task<bool> Register(RegisterRequest request);
+    }
+}
---
+++ eShopSolution.ViewModels/System/Logins/RegisterRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.ViewModels.System.Logins
+{
+    public class RegisterRequest
+    {
+        // trong tk Register này chứa các thuộc tính muốn người dùng nhập vào
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
